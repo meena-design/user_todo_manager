3205e33c40a2a255b1c294cae2aa89d557b97e59
meena <meenabhagavathula100@gmail.com>
2021-09-30 11:58:14 +0530

    Render list of todos in /todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,6 @@ class TodosController < ApplicationController
   def index
     #render plain: "Hello, this is /todos!"
     #render plain: "Hello to Rails! #{DateTime.now.to_s(:short)}"
-    render plain: Todo.all.
+    render plain: Todo.all.to_a
   end
 end
