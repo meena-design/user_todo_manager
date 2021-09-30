86a43f77a57c6bfc7a5cce53d2f943b8f6fffc00
meena <meenabhagavathula100@gmail.com>
2021-09-30 12:33:23 +0530

    todo list in pleasant format as per the tutorial

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,7 @@ class TodosController < ApplicationController
   def index
     #render plain: "Hello, this is /todos!"
     #render plain: "Hello to Rails! #{DateTime.now.to_s(:short)}"
-    render plain: Todo.all.to_a
+    #render plain: Todo.all.to_a
+    render plain:Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
 end
