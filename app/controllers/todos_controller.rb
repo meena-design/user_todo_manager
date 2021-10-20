c12c290e3bd3d6f6c5683a2e4a886776b11ee20e
meena <meenabhagavathula100@gmail.com>
2021-10-20 13:05:52 +0530

    User can now complete and update an existing todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -20,9 +20,13 @@ class TodosController < ApplicationController
   def create
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
-    new_to_do =  Todo.create!(todo_text: todo_text,due_date: due_date,completed: false)
-    response_text="Hey , Your new todo is Created with the id #{new_to_do.id}"
-    render plain:response_text
+    new_to_do =  Todo.create!(todo_text: todo_text,
+      due_date: due_date,
+      completed: false)
+    #response_text="Hey , Your new todo is Created with the id #{new_to_do.id}"
+    #render plain:response_text
+    redirect_to todos_path
+
   end
   def update
     id = params[:id]
@@ -30,6 +34,8 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain:"Updated to do completed status #{completed}"
+    #render plain:"Updated to do completed status #{completed}"
+
+    redirect_to todos_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :todos
-  resources :users
-  #get "/" => "home#index"
-  post "/users/login", to:"users#login"
+  #resources :users
+  get "/" => "home#index"
+  #post "/users/login", to:"users#login"
 end
